7e7c79af3821759881e0b959885d65ba4fc4a8d9
Markus Stefanko <markus.stefanko@gmail.com>
2017-07-04 13:31:23 +0700

    plugin rb

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-helpers
+# about: Adding more handlebars helpers to Discourse
+# version: 0.0.1
+# authors: Markus
+# url: https://github.com/mastef/discourse-helpers/
+
+register_asset "javascripts/discourse/helpers/gte.js.es6"
